bbcc3a2f1ca3764817b37ce09f5cf3361adb4edd
spinloop <spinloop@gmail.com>
2019-04-11 15:35:29 -0400

    add resolver for has-many association

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,14 +6,18 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql
 
 // temp data
 var books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
   { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' }
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Hoer of Ages', genre: 'Fantasy', id: '3', authorId: '2' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '3', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '3', authorId: '3' },
 ]
 
 var authors = [
@@ -44,7 +48,12 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: { type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id })
+      }
+    }
   })
 })
 
@@ -101,6 +110,16 @@ sample queries:
     name
     age
     id
+  }
+}
+
+{
+  author(id: 2) {
+    name
+    books {
+      name
+      genre
+    }
   }
 }
 
