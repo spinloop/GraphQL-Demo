6b96301fc5b701cea154a196c0f3012bbe8224dc
spinloop <spinloop@gmail.com>
2019-04-11 11:42:10 -0400

    create root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,8 @@
 const graphql = require('graphql')
 
-const { GraphQLObjectType, GraphQLString } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
+// NOTE: `fields` here is a function which allows `AuthorType` to be used before it's created
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
@@ -9,4 +10,22 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
+})
+
+// NOTE: `fields` here is an object
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
