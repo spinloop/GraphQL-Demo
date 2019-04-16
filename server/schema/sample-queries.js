6e7ca63cc21c4b5b7556ad242fb98a1ab44cf855
spinloop <spinloop@gmail.com>
2019-04-16 18:09:50 -0400

    create sample query file

---
+++ server/schema/sample-queries.js
@@ -0,0 +1,93 @@
+/*
+
+// Book#index
+{
+  books {
+    name
+  }
+}
+
+// Book#show
+{
+	book(id: 1) {
+    name
+    genre
+    id
+  }
+}
+
+{
+  book(id: "5cb0b86f25db32053bdc7a99") {
+    name
+    id
+    genre
+  }
+}
+
+// three levels of nesting: book -> author -> books
+{
+  book(id: "5cb0b86f25db32053bdc7a99") {
+    name
+    genre
+    author {
+      name
+      age
+      books {
+        name
+        genre
+      }
+    }
+  }
+}
+
+// Book#show with association
+{
+	book(id: 2) {
+    name
+    genre
+    author {
+      name
+      age
+    }
+  }
+}
+
+// Author#index with association
+{
+  authors {
+    name
+    books {
+      name
+      genre
+    }
+  }
+}
+
+// Author#show
+{
+	author(id: 2) {
+    name
+    age
+    id
+  }
+}
+
+// Author#show with association
+{
+  author(id: 2) {
+    name
+    books {
+      name
+      genre
+    }
+  }
+}
+
+// mutations
+mutation {
+  addAuthor(name: "Chris", age: 38) {
+  	name
+  }
+}
+
+*/
