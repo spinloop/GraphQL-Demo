a434cafe63278b03f0e5845f21f4fe621626e5ef
spinloop <spinloop@gmail.com>
2019-04-22 17:12:37 -0400

    [client] refetch query after mutation

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql, compose } from 'react-apollo'
-import { getAuthorsQuery, addBookMutation } from '../queries'
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries'
 
 class AddBook extends Component {
   constructor(props) {
@@ -32,7 +32,8 @@ class AddBook extends Component {
         name: name,
         genre: genre,
         authorId: authorId
-      }
+      },
+      refetchQueries: [{ query: getBooksQuery }]
     })
   }
 
