029a1d908fa33349ce7e1d1bf6a040a53a54080b
spinloop <spinloop@gmail.com>
2019-04-22 09:54:41 -0400

    [client] add gql query
    query constructed via gql``

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost'
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`
 
 class BookList extends Component {
   render() {
